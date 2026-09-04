ab6a0c73255c4a8693d5b40643b43abbe1a3089c
Iman Kebour <kebouriman6@gmail.com>
2026-09-04 14:04:48 +0200

    Correct BASE_URL in api.js
    Fix BASE_URL formatting issue by removing an extra slash.

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = "https://m6i5hfjwjnu3z3mmiihuwbuhwa0izria.lambda-url.eu-north-1.on.aws";
+const BASE_URL = "https:/m6i5hfjwjnu3z3mmiihuwbuhwa0izria.lambda-url.eu-north-1.on.aws";
 
 async function request(path, options = {}) {
   const response = await fetch(`${BASE_URL}${path}`, {
